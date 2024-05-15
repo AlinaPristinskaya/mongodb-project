de2adbb24c7514963d344177bfad54ec3ff15a4d
Alina Prystynska <pristinskayaalina9@gmail.com>
2024-05-15 21:06:41 +0100

    update api

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://mongodb-project-b5c380823b06.herokuapp.com/cars'
+    baseURL: 'https://mongodb-project-b5c380823b06.herokuapp.com/'
 });
 
 export default instance;
